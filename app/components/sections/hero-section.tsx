3dc6a6dd60057afaa2527ba307e03964dfb14026
lwev <106478077+lwev@users.noreply.github.com>
2025-06-20 16:39:32 +0200

    “update80”

--- app/components/sections/hero-section.tsx
+++ app/components/sections/hero-section.tsx
@@ -86,6 +86,7 @@ export default function HeroSection() {
                       })
                     }
                   }}
+                  
                 >
                   Wichtige Informationen
                 </Button>
